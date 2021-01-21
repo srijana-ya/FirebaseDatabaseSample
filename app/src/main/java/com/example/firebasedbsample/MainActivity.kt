b64b9a0e2cfb67231a18e10dd0b1e832f8900e2e
SrijanaB <58282829+srijana-ya@users.noreply.github.com>
2021-01-21 12:12:50 +0530

    Spinner added with values stored on Firebase

--- app/src/main/java/com/example/firebasedbsample/MainActivity.kt
+++ app/src/main/java/com/example/firebasedbsample/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.firebasedbsample
 
-import android.app.ProgressDialog.show
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RatingBar
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var editTextName: EditText
     lateinit var ratingBar: RatingBar
     lateinit var buttonSave: Button
+    lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,30 +21,54 @@ class MainActivity : AppCompatActivity() {
         ratingBar = findViewById(R.id.ratingBar)
         buttonSave = findViewById(R.id.buttonSave)
 
-        buttonSave.setOnClickListener {
-            saveHero()
+        spinner = findViewById(R.id.spinner)
+        val languages: MutableList<String> = ArrayList()
+        languages.add("Java")
+        languages.add("Kotlin")
 
+        val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, languages)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>,
+                                        view: View, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity,
+                        getString(R.string.selected_item) + " " +
+                                "" + languages[position], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
         }
 
+        buttonSave.setOnClickListener {
+            saveHero() }
     }
-    private fun saveHero(){
-        val name = editTextName.text.toString().trim()
 
-        if (name.isEmpty()){
+    private fun saveHero() {
+        val name = editTextName.text.toString().trim()
+        val text = spinner.selectedItem.toString()
+
+        if (name.isEmpty()) {
             editTextName.error = "Please enter a name"
             return
         }
 
-        val ref = FirebaseDatabase.getInstance().getReference("heroes")
+        val ref = FirebaseDatabase.getInstance().getReference("heroes").child(text)
         val heroId = ref.push().key
-        val hero = heroId?.let { Hero (it, name, ratingBar.numStars) }
+        val hero = heroId?.let { Hero(it, name, ratingBar.numStars) }
 
 
         if (heroId != null) {
-            ref.child(heroId).setValue(hero).addOnCompleteListener{
-                Toast.makeText(applicationContext, "Hero Saved Successfully",Toast.LENGTH_SHORT).show()
+            ref.child(heroId).setValue(hero).addOnCompleteListener {
+                Toast.makeText(applicationContext, "Hero Saved Successfully", Toast.LENGTH_SHORT).show()
 
             }
         }
+
     }
 }
